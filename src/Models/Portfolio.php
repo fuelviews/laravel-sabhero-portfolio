557fbdd72ebd416f2e116290e749d08eea52fd86
thejmitchener <thejmitchener@gmail.com>
2025-06-18 16:50:07 -0400

    Add the filament/forms dependency to the composer configuration and remove an unnecessary blank line in the Portfolio model for improved code clarity.

--- src/Models/Portfolio.php
+++ src/Models/Portfolio.php
@@ -11,6 +11,7 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
+
 class Portfolio extends Model implements HasMedia
 {
     use HasFactory;
